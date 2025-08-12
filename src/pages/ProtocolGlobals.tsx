26e0cb19701bd534159597c6c92e8ed6698eee10
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-08-12 13:33:50 +0300

    Extract Dashboard styled component into shared component

--- src/pages/ProtocolGlobals.tsx
+++ src/pages/ProtocolGlobals.tsx
@@ -1,39 +1,9 @@
-import {CardMedia} from '@mui/material'
 import {useWindowDimensions} from '../hooks/useWindowDimensions'
-import {theme} from '../theme/theme'
-import styled from '@emotion/styled'
 import {CHAIN_ID_LOCAL_STORAGE_KEY, DEFAULT_CHAINID} from '../constants/chains'
 import {SRC_PROTOCOL_GLOBALS} from '../constants/grafanaLinks'
 import {useEffect, useState} from 'react'
 import {useStoredChainId} from '../hooks/useStoredChainId'
-
-type DashboardProps = {
-  component: string
-  width: number
-  height: number
-  mobilewidth: number
-  mobileheight: number
-}
-
-const Dashboard = styled(CardMedia)<DashboardProps>(
-  {
-    border: 'none',
-    padding: '0 24px 24px',
-
-    [theme.breakpoints.down('sm')]: {
-      padding: '0',
-    },
-  },
-  props => ({
-    width: props.width,
-    height: props.height,
-    [theme.breakpoints.down('sm')]: {
-      width: props.mobilewidth,
-      height: props.mobileheight,
-    },
-  }),
-)
-
+import {Dashboard} from '../components/Dashboard'
 const ProtocolGlobals = () => {
   const storedChainId = useStoredChainId(CHAIN_ID_LOCAL_STORAGE_KEY)
   const dashboardSrc = storedChainId !== null ? SRC_PROTOCOL_GLOBALS[Number(storedChainId)] : SRC_PROTOCOL_GLOBALS[DEFAULT_CHAINID]
